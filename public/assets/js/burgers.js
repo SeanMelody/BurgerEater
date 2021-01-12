42e52793a5d0ca4d6fdfae52a1429fdfd95ea6d8
Sean Melody <seandmelody@gmail.com>
2021-01-12 12:26:48 -0800

    Eating Burger Button working, YEEEEEAY

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -12,6 +12,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
         eatBurger.forEach((button) => {
             button.addEventListener("click", (e) => {
                 console.log("button working");
+                const id = e.target.getAttribute("data-id");
+                const notDevoured = e.target.getAttribute("data-not-devoured");
+                console.log(id);
+                const newDevoured = {
+                    devoured: notDevoured,
+                };
+
+                fetch(`/api/burgers/${id}`, {
+                    method: "PUT",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(newDevoured),
+                }).then((response) => {
+                    if (response.ok) {
+                        console.log(`Burger Eaten: ${newDevoured}`)
+                        location.reload('/');
+                    } else {
+                        alert('something went wrong!');
+                    }
+                })
+
             })
         })
     }

9854ab1c7256dbe78d26a189bc603d8c872b21bd
Sean Melody <seandmelody@gmail.com>
2021-01-12 11:49:42 -0800

    typos fixed in functions, whoops

--- models/burger.js
+++ models/burger.js
@@ -3,13 +3,13 @@ const orm = require("../config/orm.js")
 
 const burger = {
     selectAll(cb) {
-        orm.all("burgers", (res) => cb(res));
+        orm.selectAll("burgers", (res) => cb(res));
     },
     insertOne(cols, vals, cb) {
-        orm.create("burgers", cols, vals, (res) => cb(res));
+        orm.insertOne("burgers", cols, vals, (res) => cb(res));
     },
     updateOne(objColVal, condition, cb) {
-        orm.update("burgers", objColVal, condition, (res) => cb(res));
+        orm.updateOne("burgers", objColVal, condition, (res) => cb(res));
     },
 };
 

3ba759b7b2b144d296a5667f509b545b7fb141fd
Sean Melody <seandmelody@gmail.com>
2021-01-12 09:11:38 -0800

    orm file set up

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+// Import MySQL connection.
+const { query } = require('./connection.js');
+const connection = require('./connection.js');
+
+// Methods Needed?
+
+// selectAll()
+
+// insertOne()
+
+// updateOne()
+
+// Gotta Module.Exports
+module.exports = orm;

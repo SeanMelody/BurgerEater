5a99e7805fc18d4fd59062734367ca97129f0baa
Sean Melody <seandmelody@gmail.com>
2021-01-12 22:26:16 -0800

    optimized for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,24 @@
 // Const for MySQL!
 const mysql = require("mysql");
 
-// Connect to the Database!
-const connection = mysql.createConnection({
-    host: 'localhost',
-    // My port
-    port: 3306,
-    // my username
-    user: 'root',
-    // my super secret password
-    password: 'password',
-    // Use the burgers_db database
-    database: 'burgers_db',
-});
+//JawsDB for Heroku
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Connect to the Database!
+    connection = mysql.createConnection({
+        host: 'localhost',
+        // My port
+        port: 3306,
+        // my username
+        user: 'root',
+        // my super secret password
+        password: 'password',
+        // Use the burgers_db database
+        database: 'burgers_db',
+    });
+}
 
 // Make the connection.
 connection.connect((err) => {
